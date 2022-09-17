1bb938a1e8ed8904d8ad92fc4c34870dbed3463e
rafaelgust <rafael.appdevs@gmail.com>
2022-09-17 11:09:13 -0300

    slider

--- lib/main.dart
+++ lib/main.dart
@@ -27,25 +27,57 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-Offset _offset = Offset.zero;
-
 class _MyHomePageState extends State<MyHomePage> {
+  double _rx = 0.0, _ry = 0.0, _rz = 0.0;
+
+  _change(double value, {required int type}) {
+    if (type == 0) {
+      setState(() => _rx = value);
+    } else if (type == 1) {
+      setState(() => _ry = value);
+    } else if (type == 2) {
+      setState(() => _rz = value);
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onPanUpdate: (details) {
-          setState(() => _offset += details.delta);
-        },
-        child: Transform(
-          transform: Matrix4.identity()
-            ..setEntry(3, 2, 0.001)
-            ..rotateX(_offset.dy * pi / 180)
-            ..rotateY(_offset.dx * pi / 180),
-          alignment: Alignment.center,
-          child: const Center(
-            child: Cube3D(),
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateX(_ry)
+                ..rotateY(_rx)
+                ..rotateZ(_rz),
+              alignment: Alignment.center,
+              child: const Cube3D(),
+            ),
+            const Spacer(),
+            Slider(
+              min: pi * -2,
+              max: pi * 2,
+              value: _rx,
+              onChanged: (value) => _change(value, type: 0),
+            ),
+            Slider(
+              min: pi * -2,
+              max: pi * 2,
+              value: _ry,
+              onChanged: (value) => _change(value, type: 1),
+            ),
+            Slider(
+              min: pi * -2,
+              max: pi * 2,
+              value: _rz,
+              onChanged: (value) => _change(value, type: 2),
+            ),
+          ],
         ),
       ),
     );
@@ -83,11 +115,9 @@ class Cube3D extends StatelessWidget {
           ),
         ),
 
-        ///Sarboard
+        ///FRONT
         Transform(
-          transform: Matrix4.identity()
-            ..translate(100.0, 0.0, 0.0)
-            ..rotateY(-pi / 2),
+          transform: Matrix4.identity()..translate(0.0, 0.0, -100.0),
           alignment: Alignment.center,
           child: Container(
             color: Colors.black,
@@ -95,9 +125,11 @@ class Cube3D extends StatelessWidget {
           ),
         ),
 
-        ///FRONT
+        ///STARBOARD
         Transform(
-          transform: Matrix4.identity()..translate(0.0, 0.0, -100.0),
+          transform: Matrix4.identity()
+            ..translate(100.0, 0.0, 0.0)
+            ..rotateY(-pi / 2),
           alignment: Alignment.center,
           child: Container(
             color: Colors.black,
